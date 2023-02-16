71a98f322f44c14d522ad84e6fe1577199c1cbec
Claire Lovisa <lovisa.claire@gmail.com>
2023-02-16 13:48:26 +0100

    Update ingest graph

--- config/worship-services-sensitive-consumer/positions-dispatching/config.js
+++ config/worship-services-sensitive-consumer/positions-dispatching/config.js
@@ -7,11 +7,10 @@ const MAX_REASONING_RETRY_ATTEMPTS = parseInt(process.env.MAX_REASONING_RETRY_AT
 const SLEEP_BETWEEN_BATCHES = parseInt(process.env.SLEEP_BETWEEN_BATCHES || 1000);
 const SLEEP_TIME_AFTER_FAILED_REASONING_OPERATION = parseInt(process.env.SLEEP_TIME_AFTER_FAILED_REASONING_OPERATION || 10000);
 const SLEEP_TIME_AFTER_FAILED_DB_OPERATION = parseInt(process.env.SLEEP_TIME_AFTER_FAILED_DB_OPERATION || 60000);
-const INGEST_GRAPH = process.env.INGEST_GRAPH || `http://mu.semte.ch/graphs/organisatieportaal`;
 
-if (!process.env.FILE_SYNC_GRAPH)
-  throw `Expected 'FILE_SYNC_GRAPH' to be provided.`;
-const FILE_SYNC_GRAPH = process.env.FILE_SYNC_GRAPH;
+if (!process.env.INGEST_GRAPH)
+  throw `Expected 'INGEST_GRAPH' to be provided.`;
+const INGEST_GRAPH = process.env.INGEST_GRAPH;
 
 module.exports = {
   BATCH_SIZE,
@@ -23,6 +22,5 @@ module.exports = {
   SLEEP_BETWEEN_BATCHES,
   SLEEP_TIME_AFTER_FAILED_DB_OPERATION,
   SLEEP_TIME_AFTER_FAILED_REASONING_OPERATION,
-  INGEST_GRAPH,
-  FILE_SYNC_GRAPH
+  INGEST_GRAPH
 };
